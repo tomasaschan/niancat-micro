ab02ac801a77ea12b34541fe4992d139ac678c76
Erik Edin <erikedin.name@gmail.com>
2017-05-01 10:31:56 +0200

    add tests that do not compile

---
+++ src/main/scala/io/github/dandeliondeathray/niancat/PuzzleEngine.scala
@@ -0,0 +1,47 @@
+package io.github.dandeliondeathray.niancat
+
+/**
+  * Created by Erik Edin on 2017-04-30.
+  */
+
+/** A Puzzle is a String of exactly nine characters. */
+case class Puzzle(letters: String)
+
+/** A Word is a potential solution to a Puzzle, but can be any length. */
+case class Word(letters: String)
+
+/** A User is of course a user in the chat room. */
+case class User(name: String)
+
+class PuzzleEngine {
+  def set(puzzle: Puzzle): Response = DummyReply()
+  def get(): Response = DummyReply()
+}
+
+/** A PuzzleCommand is a command sent by a user to Niancat. */
+sealed trait PuzzleCommand {
+  def apply(engine: PuzzleEngine)
+}
+
+case class Set(puzzle: Puzzle) extends PuzzleCommand {
+  def apply(engine: PuzzleEngine): Unit = {
+    engine.set(puzzle)
+  }
+}
+
+case class Get() extends PuzzleCommand {
+  def apply(engine: PuzzleEngine) = engine.get()
+}
+
+//case class CheckSolution(word: Word, user: User) extends PuzzleCommand
+//case class AddUnsolution(unsolution: String, user: User) extends PuzzleCommand
+//case class ListUnsolutions(user: User) extends PuzzleCommand
+
+trait Response
+trait Reply extends Response
+trait Notification extends Response
+
+case class NoPuzzleSet() extends Reply
+case class DummyReply() extends Reply
+
+
---
+++ src/test/scala/io/github/dandeliondeathray/niancat/PuzzleEngineSpec.scala
@@ -0,0 +1,14 @@
+package io.github.dandeliondeathray.niancat
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by Erik Edin on 2017-05-01.
+  */
+class PuzzleEngineSpec extends FlatSpec with Matchers {
+  "When getting a puzzle which is not set, the engine" should "reply that it's not set" in {
+    var engine = PuzzleEngine()
+    val response = Get()(engine)
+    response should be NoPuzzleSet()
+  }
+}
