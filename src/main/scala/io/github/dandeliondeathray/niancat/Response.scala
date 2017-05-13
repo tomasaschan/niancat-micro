03e6e236894f5f050f7ec82fa004a4ea73a9b30f
Erik Edin <erikedin.name@gmail.com>
2017-05-13 16:44:53 +0200

    write lots of responses

--- src/main/scala/io/github/dandeliondeathray/niancat/Response.scala
+++ src/main/scala/io/github/dandeliondeathray/niancat/Response.scala
@@ -1,5 +1,21 @@
 package io.github.dandeliondeathray.niancat
 
+object DisplayHelper {
+  implicit class PuzzleDisplay(p: Puzzle) {
+    def show: String = p.letters grouped(3) mkString(" ")
+  }
+
+  implicit class SolutionResultDisplay(s: SolutionResult) {
+    def display: Seq[String] = s.wordsAndSolvers.map(kv => showWordAndSolution(kv._1, kv._2)) toSeq
+
+    private def showWordAndSolution(w: Word, solvers: Seq[User]): String = {
+      s"**${w.letters}**: " ++ solvers.map(_.name).mkString(", ")
+    }
+  }
+}
+
+import DisplayHelper._
+
 sealed trait Response
 sealed trait Reply extends Response
 sealed trait Notification extends Response
@@ -7,29 +23,52 @@ sealed trait Notification extends Response
 case class CompositeResponse(responses: Vector[Response]) extends Response
 case class NoResponse() extends Response
 
-case class NoPuzzleSet() extends Reply
-case class GetReply(puzzle: Puzzle) extends Reply {
-  override def toString: String = PuzzleHelper.displayPuzzle(puzzle)
+case class NoPuzzleSet() extends Reply {
+  override def toString: String = "Nian är inte satt."
 }
-case class NotInTheDictionary(word: Word) extends Reply
-case class CorrectSolution(word: Word) extends Reply
-case class WordAndPuzzleMismatch(word: Word, puzzle: Puzzle) extends Reply
-case class IncorrectLength(word: Word) extends Reply
-case class InvalidPuzzle(puzzle: Puzzle) extends Reply
-case class InvalidCommandReply(msg: String, invalidCommandError: InvalidCommandError) extends Reply
-case class SamePuzzle(puzzle: Puzzle) extends Reply
+case class GetReply(puzzle: Puzzle) extends Reply {
+  override def toString: String = puzzle show
+}
+case class NotInTheDictionary(word: Word) extends Reply {
+  override def toString: String = s"Ordet ${word.letters} finns inte med i SAOL."
+}
+case class CorrectSolution(word: Word) extends Reply {
+  override def toString: String = s"Ordet ${word.letters} är korrekt!"
+}
+case class WordAndPuzzleMismatch(word: Word, puzzle: Puzzle) extends Reply {
+  override def toString: String = s"Ordet ${word.letters} matchar inte nian ${puzzle show}"
+}
+case class IncorrectLength(word: Word) extends Reply {
+  override def toString: String = s"${word.letters} är inte nio tecken långt."
+}
+case class InvalidPuzzle(puzzle: Puzzle) extends Reply {
+  override def toString: String = s"Pusslet ${puzzle show} är inte giltigt!"
+}
+case class InvalidCommandReply(msg: String, invalidCommandError: InvalidCommandError) extends Reply {
+  override def toString = {
+    invalidCommandError match {
+      case UnknownCommand(cmd: String) => s"Okänt kommando $cmd"
+      case WrongArguments(actual: Int, expected: Int) =>
+        s"Fel antal argument. Fick $actual men förväntade $expected"
+    }
+  }
+}
+case class SamePuzzle(puzzle: Puzzle) extends Reply {
+  override def toString = s"Pusslet ${puzzle show} är redan satt!"
+}
 
 case class NewPuzzle(puzzle: Puzzle) extends Notification {
   override def toString: String = {
-    val puzzleString = PuzzleHelper.displayPuzzle(puzzle)
-    s"Dagens nian är $puzzleString"
+    s"Dagens nian är ${puzzle show}"
   }
 }
-case class YesterdaysPuzzle(result: SolutionResult) extends Notification
-case class MultipleSolutions(n: Int) extends Notification
+case class YesterdaysPuzzle(result: SolutionResult) extends Notification {
+  override def toString: String = {
+    val lines = Seq("**Gårdagens lösningar:**") ++ result.display
 
-object PuzzleHelper {
-  def displayPuzzle(p: Puzzle): String = {
-    p.letters grouped(3) mkString(" ")
+    lines mkString("\n")
   }
+}
+case class MultipleSolutions(n: Int) extends Notification {
+  override def toString = "Dagens nian har $n lösningar."
 }
---
+++ src/test/scala/io/github/dandeliondeathray/niancat/ResponseSpec.scala
@@ -0,0 +1,31 @@
+package io.github.dandeliondeathray.niancat
+
+import org.scalatest._
+
+class ResponseSpec extends FlatSpec with Matchers {
+  "NoPuzzleSet" should "say there's no puzzle set" in {
+    NoPuzzleSet().toString should include ("Nian är inte satt.")
+  }
+
+  "NotInTheDictionary" should "say a word isn't in the dictionary" in {
+    NotInTheDictionary(Word("ABC")).toString should include ("ABC finns inte med i SAOL")
+  }
+
+  "CorrectSolution" should "say a solution is korrekt" in {
+    CorrectSolution(Word("DEF GHI")).toString should include ("DEF GHI är korrekt")
+  }
+
+  "YesterdaysPuzzle" should "list all solutions and user names" in {
+    val solutionResult = SolutionResult(Map(
+      Word("ABCDEFGHI") -> Seq(User("foo"), User("bar")),
+      Word("DEFGHIABC") -> Seq(User("baz"))
+    ))
+
+    val yesterdaysAsString = YesterdaysPuzzle(solutionResult).toString
+
+    yesterdaysAsString should include ("**ABCDEFGHI**:")
+    yesterdaysAsString should include ("**DEFGHIABC**:")
+    yesterdaysAsString should include ("foo, bar")
+    yesterdaysAsString should include ("baz")
+  }
+}
