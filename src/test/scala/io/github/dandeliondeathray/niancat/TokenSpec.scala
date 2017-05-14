018269318c5486dca7aba4e5878def41c8377292
Erik Edin <erikedin.name@gmail.com>
2017-05-14 23:32:22 +0200

    implement token handler

--- src/test/scala/io/github/dandeliondeathray/niancat/TokenSpec.scala
+++ src/test/scala/io/github/dandeliondeathray/niancat/TokenSpec.scala
@@ -1,13 +1,33 @@
 package io.github.dandeliondeathray.niancat
 
+import java.io.PrintWriter
+
 import org.scalatest._
+
+import scala.util.{Failure, Success}
 
 class TokenSpec extends FlatSpec with Matchers {
   "a Token" should "read from a dot directory in the users home" in {
     val token = "01234567889abcdef"
     // Write the file $HOME/.slack/token_integration_test.token
+    val home = System.getenv("HOME")
+    assume(home != null)
+    val slackDir = new java.io.File(s"$home/.slack")
+
+    if (!slackDir.exists()) {
+      assume(slackDir.mkdirs())
+    }
+
+    val tokenFile = new java.io.File(s"$home/.slack/token_integration_test.token")
+    val pw = new PrintWriter(tokenFile, "UTF-8")
+    pw.write(token)
+    pw.close()
 
     // Supply the name token_integration_test and expect the token back
-    Token.read("token_integration_test" shouldBe Success(token)
+    Token.read("token_integration_test") shouldBe Success(token)
+  }
+
+  it should "return failure for an unknown token" in {
+    Token.read("there_is_no_such_token") shouldBe a [Failure[_]]
   }
 }
