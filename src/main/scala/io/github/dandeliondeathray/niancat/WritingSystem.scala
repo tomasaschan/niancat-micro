9cccf97ffe25c6448f2c830d1d66327dfcb570a9
Erik Edin <erikedin.name@gmail.com>
2017-05-19 20:35:20 +0200

    fix bug regarding word length of decomposed strings

--- src/main/scala/io/github/dandeliondeathray/niancat/WritingSystem.scala
+++ src/main/scala/io/github/dandeliondeathray/niancat/WritingSystem.scala
@@ -1,7 +1,10 @@
 package io.github.dandeliondeathray.niancat
 
+import org.scalactic.NormMethods._
+import WordNormalizer._
+
 object WritingSystemHelper {
   implicit class WritingSystem(word: Word) {
-    def isNineLetters: Boolean = word.letters.codePoints().count == 9
+    def isNineLetters: Boolean = word.norm.letters.codePoints().count == 9
   }
 }
--- src/test/scala/io/github/dandeliondeathray/niancat/WritingSystemSpec.scala
+++ src/test/scala/io/github/dandeliondeathray/niancat/WritingSystemSpec.scala
@@ -1,7 +1,10 @@
 package io.github.dandeliondeathray.niancat
 
+import java.text.Normalizer
+
 import org.scalatest._
 import WritingSystemHelper._
+
 
 class WritingSystemSpec extends FlatSpec with Matchers {
 
@@ -15,5 +18,10 @@ class WritingSystemSpec extends FlatSpec with Matchers {
 
   "A normal word" should "be nine letters" in {
     Word("ABCDEFGHI").isNineLetters shouldBe true
+  }
+
+  "A word with decomposed accented characters" should "still be nine letters" in {
+    val decomposed = Normalizer.normalize("ÖVERSÄTTA", Normalizer.Form.NFKD)
+    Word(decomposed).isNineLetters shouldBe true
   }
 }
