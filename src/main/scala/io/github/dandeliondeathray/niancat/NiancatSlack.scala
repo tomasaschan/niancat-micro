2ea51d674266d587d7224408b39346fa23809b4c
Erik Edin <erikedin.name@gmail.com>
2017-05-27 16:22:59 +0200

    create only one ActorSystem and add debug output

--- src/main/scala/io/github/dandeliondeathray/niancat/NiancatSlack.scala
+++ src/main/scala/io/github/dandeliondeathray/niancat/NiancatSlack.scala
@@ -4,8 +4,9 @@ import akka.actor.ActorSystem
 import slack.rtm.SlackRtmClient
 import scala.concurrent.duration._
 
-class NiancatSlack(val token: String, val dictionary: Dictionary, notificationChannel: Channel) {
-  implicit val system = ActorSystem("slack")
+class NiancatSlack(val token: String,
+                   val dictionary: Dictionary,
+                   notificationChannel: Channel)(implicit val system: ActorSystem) {
   implicit val ec = system.dispatcher
 
   val client = SlackRtmClient(token, 20.seconds)
@@ -21,7 +22,12 @@ class NiancatSlack(val token: String, val dictionary: Dictionary, notificationCh
   val messageHandler = new MessageHandler(parser, engine, responder, sendMessage, selfUser)
 
   client.onMessage { message =>
+    println(s"onMessage: $message")
     client.state.getUserById(message.user).foreach(user =>
       messageHandler.handle(Channel(message.channel), User(user.name), message.text))
+  }
+
+  client.onEvent { event =>
+    println(s"Event: $event")
   }
 }
