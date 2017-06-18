b0f117edc9a0bb4c80bd7fdc758e845fa08bf511
Erik Edin <erikedin.name@gmail.com>
2017-06-18 15:39:41 +0200

    Dockerfiles for fake niancat service

--- fake-niancat-service/app.py
+++ fake-niancat-service/app.py
@@ -2,6 +2,6 @@
 import connexion
 from connexion.resolver import RestyResolver
 
-app = connexion.App(__name__, specification_dir='../apis')
+app = connexion.App(__name__, specification_dir='.')
 app.add_api('niancat.yaml', resolver=RestyResolver('api'))
 app.run(port=8081)
