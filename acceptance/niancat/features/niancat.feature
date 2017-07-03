c0c12b4193981daa3827b5dd3f6d52a40f6702b8
Erik Edin <erikedin.name@gmail.com>
2017-07-03 18:50:19 +0200

    working on acceptance tests for Niancat

--- acceptance/niancat/features/niancat.feature
+++ acceptance/niancat/features/niancat.feature
@@ -1,4 +1,4 @@
-Feature: Niancat as a REST service
+Feature: Niancat as a service
   In order to solve the Dagens Nian puzzle
   As a user
   I want to be able to check solutions and store unsolutions
@@ -7,10 +7,41 @@ Feature: Niancat as a REST service
   Scenario: No puzzle is set
     Given that no puzzle is set
      When I get the puzzle
-     Then I get a 404 Not Found response
+     Then I get a response that the puzzle is not set
 
   Scenario: Get a puzzle
-    Given that I set the puzzle ABCDEFGHI
+    Given I set the puzzle DATORSPLE
      When I get the puzzle
-     Then I get back "ABCDEFGHI"
+     Then I get back "DATORSPLE"
 
+  Scenario: Solve a puzzle
+    Given I set the puzzle DATORSPLE
+     When I test the solution DATORSPEL
+     Then the response is that it is correct
+      And a notification that I solved the puzzle
+
+  Scenario: Check an incorrect solution
+    Given I set the puzzle DATORSPLE
+     When I test the solution DATORLESP
+     Then the response is that it is incorrect
+
+  Scenario: Solutions are reported on next puzzle
+    Given I set the puzzle DATORSPLE
+      And I test the solution DATORSPEL
+     When I set the puzzle VANTRIVSA
+     Then my name is listed as solving DATORSPEL in the notification channel
+
+  Scenario: A notification is sent when a puzzle with multiple solutions is set
+     When I set the puzzle DATORSPLE
+     Then there is a notification that there are two solutions
+
+  Scenario: Setting an unsolution
+    Given I store an unsolution "Unsolution 1"
+     When I list my unsolutions
+     Then the unsolutions contain "Unsolution 1"
+
+  Scenario: Unsolutions are listed on next puzzle
+    Given I set the puzzle DATORSPLE
+      And I store an unsolution "ABC DEF GHI"
+     When I set the puzzle VANTRIVSA
+     Then the unsolution "ABC DEF GHI" is listed with my name
--- acceptance/niancat/features/steps/niancat.py
+++ acceptance/niancat/features/steps/niancat.py
@@ -16,12 +16,12 @@ def step_impl(context):
     context.response = requests.get(urljoin(niancat, '/puzzle'))
 
 
-@then(u'I get a 404 Not Found response')
+@then(u'I get a response that the puzzle is not set')
 def step_impl(context):
     assert context.response.status_code == requests.code.not_found
 
 
-@given(u'that I set the puzzle {puzzle}')
+@given(u'I set the puzzle {puzzle}')
 def step_impl(context, puzzle):
     context.response = requests.post(urljoin(niancat, '/puzzle'), json=puzzle)
 
@@ -29,4 +29,71 @@ def step_impl(context, puzzle):
 @then(u'I get back {response_text}')
 def step_impl(context, response_text):
     assert context.text == response_text
+
+
+@then(u'the response is that it is correct')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the response is that it is correct')
+
+
+@then(u'a notification that I solved the puzzle')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then a notification that I solved the puzzle')
+
+
+@when(u'I test the solution {maybesolution}')
+def step_impl(context, maybesolution):
+    raise NotImplementedError(u'STEP: When I test the solution DATORLESP')
+
+
+@then(u'the response is that it is incorrect')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the response is that it is incorrect')
+
+
+@then(u'my name is listed as solving {solution} in the notification channel')
+def step_impl(context, solution):
+    raise NotImplementedError(u'STEP: Then my name is listed as solving DATORSPEL in the notification channel')
+
+
+@then(u'there is a notification that there are two solutions')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then there is a notification that there are two solutions')
+
+
+@given(u'I store an unsolution "{unsolution}"')
+def step_impl(context, unsolution):
+    user_id = "U012345"
+    context.response = requests.post(
+        urljoin(niancat, '/unsolution/{}'.format(user_id)),
+        json=unsolution)
+
+
+@when(u'I list my unsolutions')
+def step_impl(context):
+    user_id = "U012345"
+    context.response = requests.get(urljoin(niancat, '/unsolution/{}'.format(user_id)))
+
+
+@then(u'the unsolutions contain "{unsolution}"')
+def step_impl(context, unsolution):
+    assert unsolution in context.response.text
+
+
+@then(u'the unsolution "{unsolution}" is listed with my name')
+def step_impl(context, unsolution):
+    user_id = "U012345"
+    assert user_id in context.response.text
+    assert unsolution in context.response.text
+
+
+@given(u'I test the solution {maybesolution}')
+def step_impl(context, maybesolution):
+    raise NotImplementedError(u'STEP: Given I test the solution DATORSPEL')
+
+
+@when(u'I set the puzzle {puzzle}')
+def step_impl(context, puzzle):
+    context.response = requests.post(urljoin(niancat, '/puzzle'), json=puzzle)
+
 
