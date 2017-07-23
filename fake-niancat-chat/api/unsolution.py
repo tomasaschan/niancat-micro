6f5e0926e651b0de651b2429f280373d30816f20
Erik Edin <erikedin.name@gmail.com>
2017-07-23 18:50:03 +0200

    remove obsolete fake service

--- fake-niancat-chat/api/unsolution.py
+++
@@ -1,8 +0,0 @@
-def get(user):
-    print("GET /unsolution/{}".format(user))
-    return {"response_type": "reply", "message": "Tack för att du är så fin. Så säger vi om **olösningar**."}
-
-
-def post(user, unsolution):
-    print("POST /unsolution/{}:".format(user), unsolution)
-    return {"response_type": "reply", "message": "Satte olösning {}".format(unsolution)}
--- fake-niancat-chat/app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import connexion
-from connexion.resolver import RestyResolver
-
-app = connexion.App(__name__, specification_dir='.')
-app.add_api('niancat-chat.yaml', resolver=RestyResolver('api'))
-app.run(port=8081)
