322c132aac3892aa96dc285f8e2eb8ac7e77dc01
Erik Edin <erikedin@users.noreply.github.com>
2017-10-29 07:56:17 +0100

    remove acceptance test directory
    because it's like fetch. it never happened.

--- acceptance/niancat/features/environment.py
+++
@@ -1,5 +0,0 @@
-import time
-
-def before_feature(self, feature):
-    # Sleep to allow the niancat service to start up
-    time.sleep(5)
--- acceptance/niancat/features/steps/niancat.py
+++
@@ -1,3 +0,0 @@
-from behave import *
-from hamcrest import *
-
