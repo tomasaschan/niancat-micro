da63b86523cc537cfe5e8265b45de88b64525b4d
tomasaschan <1550920+tomasaschan@users.noreply.github.com>
2019-10-30 22:53:18 +0100

    Extract state parameters to serializable trait

--- niancat/src/main/scala/io/github/dandeliondeathray/niancat/NiancatState.scala
+++ niancat/src/main/scala/io/github/dandeliondeathray/niancat/NiancatState.scala
@@ -21,61 +21,63 @@ trait State {
   def storeUnconfirmedUnsolution(user: User, text: String)
 }
 
-class NiancatState extends State {
-  private var _puzzle: Option[Puzzle] = None
-  private var _unsolutions: Map[User, Seq[String]] = Map()
-  private var _unconfirmedUnsolutions: Map[User, String] = Map()
-  private var solutions: Seq[(Word, User)] = Seq()
-  private var streaks: Map[User, Int] = Map()
+trait StateRepr {
+  var puzzle: Option[Puzzle]
+  var unsolutions: Map[User, Seq[String]]
+  var unconfirmedUnsolutions: Map[User, String]
+  var solutions: Seq[(Word, User)]
+  var streaks: Map[User, Int]
+}
 
-  override def puzzle(): Option[Puzzle] = _puzzle
+class NiancatState(var repr: StateRepr) extends State {
+  override def puzzle(): Option[Puzzle] = repr.puzzle
 
-  override def unsolutions() = _unsolutions
+  override def unsolutions() = repr.unsolutions
 
-  override def unconfirmedUnsolutions() = _unconfirmedUnsolutions
+  override def unconfirmedUnsolutions() = repr.unconfirmedUnsolutions
 
   override def result(allSolutions: Seq[Word]): Option[SolutionResult] = {
     puzzle match {
       case None => None
       case Some(p) => {
         val allSolutionsMap: Map[Word, Seq[User]] = (allSolutions map (_ -> Seq[User]())).toMap
-        val foundSolutions: Map[Word, Seq[User]] = (solutions groupBy (_._1) mapValues (_.map(_._2).toSeq))
-        Some(SolutionResult(allSolutionsMap ++ foundSolutions, streaks.toMap))
+        val foundSolutions: Map[Word, Seq[User]] = (repr.solutions groupBy (_._1) mapValues (_.map(_._2).toSeq))
+        Some(SolutionResult(allSolutionsMap ++ foundSolutions, repr.streaks.toMap))
       }
     }
   }
 
   override def reset(p: Puzzle, isWeekday: Boolean): Unit = {
-    _puzzle = Some(p.norm)
+    repr.puzzle = Some(p.norm)
     if (isWeekday) {
-      streaks = streaks filter { case (user, _) => solutions.map(_._2) contains user }
+      repr.streaks = repr.streaks filter { case (user, _) => repr.solutions.map(_._2) contains user }
     }
-    solutions = Seq()
-    _unsolutions = Map()
-    _unconfirmedUnsolutions = Map()
+    repr.solutions = Seq()
+    repr.unsolutions = Map()
+    repr.unconfirmedUnsolutions = Map()
   }
   override def solved(user: User, word: Word, isWeekday: Boolean): Unit = {
-    val userHasFoundThisSolutionBefore = solutions contains ((word.norm, user))
+    val userHasFoundThisSolutionBefore = repr.solutions contains ((word.norm, user))
     if (!userHasFoundThisSolutionBefore) {
-      solutions :+= (word.norm, user)
+      repr.solutions :+= (word.norm, user)
       if (isWeekday) {
-        streaks += user -> ((streaks getOrElse (user, 0)) + 1)
+        repr.streaks += user -> ((repr.streaks getOrElse (user, 0)) + 1)
       }
     }
   }
 
   override def hasSolved(user: User, word: Word): Boolean = {
-    solutions contains ((word.norm, user))
+    repr.solutions contains ((word.norm, user))
   }
-  override def streak(user: User): Int = streaks getOrElse (user, 0)
+  override def streak(user: User): Int = repr.streaks getOrElse (user, 0)
 
   override def storeUnsolution(user: User, text: String): Unit = {
-    val unsolutionsForUser: Seq[String] = _unsolutions.getOrElse(user, Seq[String]())
-    _unsolutions += (user -> (unsolutionsForUser :+ text))
-    _unconfirmedUnsolutions -= user
+    val unsolutionsForUser: Seq[String] = repr.unsolutions.getOrElse(user, Seq[String]())
+    repr.unsolutions += (user -> (unsolutionsForUser :+ text))
+    repr.unconfirmedUnsolutions -= user
   }
 
   override def storeUnconfirmedUnsolution(user: User, text: String): Unit = {
-    _unconfirmedUnsolutions += (user -> text)
+    repr.unconfirmedUnsolutions += (user -> text)
   }
 }
